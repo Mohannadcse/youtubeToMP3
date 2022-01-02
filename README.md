f9371436e4fdbdaa888309bdfda244f3042f2e8b
Mohannad Alhanahnah <mohannad.alhanahnah@gmail.com>
2022-01-01 21:09:45 -0600

    update the script to accept args

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# youtubeToMP3
+# Usage 
+To use the script, run the following commands:
+```sh
+git clone git@github.com:Mohannadcse/youtubeToMP3.git
+cd youtubeToMP3
+python3 -m venv env
+source env/bin/activate
+youtubeToMP3.py -u <url> OR youtubeToMP3.py -url=<url>
+```
+
+
--- youtubeToMP3.py
+++ youtubeToMP3.py
@@ -1,16 +1,38 @@
 from __future__ import unicode_literals
 import youtube_dl
+import getopt, sys
 
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'continue_dl': True,
-    'ignoreerrors': True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/playlist?list=PL1GJ09M00J5b-dGPyNFyKyIOsUbsgo6Ta'])
+def download_url(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'continue_dl': True,
+        'ignoreerrors': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+
+def main(argv):
+   try:
+      opts, args = getopt.getopt(argv,'hu:', ["url="])
+   except getopt.GetoptError:
+      print('youtubeToMP3.py -u <url>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('youtubeToMP3.py -u <url>')
+         print ("im HERE 2")
+         sys.exit()
+      elif opt in ("-u", "--url"):
+         download_url(arg)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
